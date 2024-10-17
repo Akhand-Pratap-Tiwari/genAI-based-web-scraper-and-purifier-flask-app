50a058afc579151b27a8938fbaed74ef209b5baa
Akhand Pratap Tiwari <akdevstudios100@gmail.com>
2024-10-17 19:08:30 +0530

    Added dummy secrets folder
    Secrets folder where to place your details for it to work. Make sure to not post real details. Keep then hidden and don't commit.

---
+++ secrets/cosmocloud_api_details.py
@@ -0,0 +1,7 @@
+environmentId = "COSMOCLOUD_ENVIRONMENT_ID_HERE"
+projectId = "COSMOCLOUD_PROJECT_ID_HERE"
+url = "COSMOCLOUD_ENDPOINT_URL_HERE"
+headers = {
+    "environmentId": environmentId,
+    "projectId": projectId,
+}
---
+++ secrets/gemini_api_details.py
@@ -0,0 +1,1 @@
+gemini_api_key = "YOUR_GEMINI_API_KEY_HERE"
